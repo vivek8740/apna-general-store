4564c48d673e8336f912a3a4d35fe58c54727b4f
Vivek <vivek.raj0538@gmail.com>
2024-03-01 19:56:43 +0530

    Initializing apna-general-store project

---
+++ src/main/java/com/apnageneralstore/ApnaGeneralStoreApplication.java
@@ -0,0 +1,13 @@
+package com.apnageneralstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApnaGeneralStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApnaGeneralStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apnageneralstore/ApnaGeneralStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apnageneralstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApnaGeneralStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
