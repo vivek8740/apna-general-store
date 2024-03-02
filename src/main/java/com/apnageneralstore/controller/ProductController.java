e7ef6663d55d690328e0446f806868ad80f1207d
Vivek <vivek.raj0538@gmail.com>
2024-03-02 11:45:15 +0530

    Adding java doc comment for Product Controller class

--- src/main/java/com/apnageneralstore/controller/ProductController.java
+++ src/main/java/com/apnageneralstore/controller/ProductController.java
@@ -1,10 +1,8 @@
 package com.apnageneralstore.controller;
 
 import com.apnageneralstore.dto.ProductDto;
-import com.apnageneralstore.repository.entity.Category;
 import com.apnageneralstore.service.CategoryService;
 import com.apnageneralstore.service.ProductService;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +10,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * Controller class for managing product-related operations.
+ */
 @RestController
 @RequestMapping("/product")
 public class ProductController {
@@ -22,6 +23,12 @@ public class ProductController {
     @Autowired
     CategoryService categoryService;
 
+    /**
+     * Retrieves a list of all products.
+     *
+     * @return ResponseEntity containing a list of ProductDto objects and HTTP status OK if successful,
+     *         or an empty list and HTTP status NOT FOUND if no products are found
+     */
     @GetMapping("/")
     public ResponseEntity<List<ProductDto>> getProducts() {
         List<ProductDto> body = productService.listProducts();
